bd39576e94ef44f4a7a8b02906b645530e798816
Eduardo Santos <eduluis.cco@gmail.com>
2023-03-31 22:25:32 -0300

    Exercicio 4 POO

--- Exercicios/POO/POO004.php
+++ Exercicios/POO/POO004.php
@@ -5,12 +5,27 @@ interface FormaGeometrica{
 }
 
 class Retangulo implements FormaGeometrica{
-    function calcularPerimetro(){
+    public $base;
+    public $altura;
 
+    public function calcularArea() {
+        return $this->base * $this->altura;
+      }
+      
+      public function calcularPerimetro() {
+        return 2 * ($this->base + $this->altura);
+      }
     }
-}
 
-class Retangulo implements FormaGeometrica{
-    
-}
+    class Circulo implements FormaGeometrica {
+        public $raio;
+        
+        public function calcularArea() {
+          return pi() * pow($this->raio, 2);
+        }
+        
+        public function calcularPerimetro() {
+          return 2 * pi() * $this->raio;
+        }
+      }
 ?>
